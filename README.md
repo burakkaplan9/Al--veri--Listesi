e55c0a22a7fd8b1917fd6569658f65059c0759e3
burakkaplan9 <burakkaplan9@yandex.com>
2024-04-18 16:43:05 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+## Alış-veriş Listesi
+
+- HTML, CSS ve JS kullanılarak tasarlanmıştır.
+
+- Kişilerin alış-verişe çıkarken alınacak ürünleri unutmaması için hayatı kolaylaştırmak için yapılmış uygulamadır.
+
+- Alış-veriş listesi düzenlenebilir, istenmeyen ürünler silinebilir alış-veriş sonunda liste tamamen tek tuşla temizlenebilir.
+
+# GİF
+
+<img src="Alış-veriş Listesi.gif">
---
+++ main.js
@@ -0,0 +1,81 @@
+const form = document.querySelector(".grocery-form");
+const grocery = document.getElementById("grocery");
+const container = document.querySelector(".grocery-container");
+const list = document.querySelector(".grocery-list");
+const alert = document.querySelector(".alert");
+const submitBtn = document.querySelector(".submit-btn");
+
+
+let editElement;
+let editFlag = false;
+let editID = ""
+
+form.addEventListener("submit", addItem);
+
+function displayAlert(text, action){
+    console.log(text, action);
+    alert.textContent = text;
+    alert.classList.add(`alert-${action}`);
+    setTimeout(() => {
+        alert.textContent = "";
+        alert.classList.remove(`alert-${action}`);
+    }, 2000);
+}
+
+function addItem(e) {
+    e.preventDefault();
+    const value = grocery.value;
+    const id = new Date().getTime().toString();
+
+    if (value !== "" && !editFlag) {
+        const element = document.createElement("article");
+        let attr = document.createAttribute("data-id");
+        attr.value = id;
+        element.setAttributeNode(attr);
+        element.classList.add("grocery-item");
+        element.innerHTML = `<p class="title">${value}</p>
+        <div class="btn-container">
+            <button type="button" class="edit-btn">
+                <i class="bi bi-pencil-square"></i>
+            </button>
+            <button type="button" class="delete-btn">
+                <i class="bi bi-trash"></i>
+            </button>
+        </div>`;
+
+        const deleteBtn = element.querySelector(".delete-btn");
+        deleteBtn.addEventListener("click", deleteItem);
+        const editBtn = element.querySelector(".edit-btn");
+        editBtn.addEventListener("click", editItem);
+
+        list.appendChild(element);
+        displayAlert("Başarıyla Eklendi", "success");
+        container.classList.add("show-container");
+        grocery.value = "";
+        
+    }else if (value !== "" && editFlag){
+        editElement.innerHTML = value;
+        displayAlert("Değer Değiştirildi", "success")
+    }else {
+    }
+    
+}
+
+function deleteItem(e){
+    const element = e.currentTarget.parentElement.parentElement;
+    const id = element.dataset.id;
+    list.removeChild(element);
+
+    displayAlert("Öğe Kaldırıldı", "danger")
+}
+
+function editItem(e){
+    const element = e.currentTarget.parentElement.parentElement;
+    editElement = e.currentTarget.parentElement.previousElementSibling;
+    grocery.value = editElement.innerHTML;
+
+    editFlag = true;
+    editID = element.dataset.id;
+    submitBtn.textContent = "Düzenle";
+
+}
